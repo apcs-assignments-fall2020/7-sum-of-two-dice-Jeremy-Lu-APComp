fb16a684ec393b70174c09508b3821d608f58126
Jeremy-Lu-APComp <21jlu@athenian.org>
2020-09-26 16:59:40 +0000

    Added code for sum of two dice project

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,24 +1,39 @@
 import java.util.Scanner;
+import java.util.Random; //imports the random module
 
 public class MyMain {
 
     // Rolls a single die
     // Returns a random integer between 1 and 6
     public static int rollDie() {
-        // REPLACE YOUR CODE HERE
-        return -1;
+        Random dice_roll = new Random();
+        int number;
+        int output = 0;
+        output = dice_roll.nextInt(6) + 1;
+        return output;
     }
 
     // Rolls two dice n times
     // This method returns an int array of size 11, where each element is
     // the number of times a certain sum of two dice was rolled
     public static int[] sumOfTwoDice(int n) {
-        // REPLACE YOUR CODE HERE
-        return null;
+        int[] arr_sum = new int[11];
+        for(int a  = 0; a < n; a++){
+            int b = rollDie() + rollDie();
+            arr_sum[b-2]++;
+        }
+        return(arr_sum);
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please input an integer: ");
+        int n = scan.nextInt();
+        int[] a = sumOfTwoDice(n);
+        for(int b = 0; b < 11; b++){
+            System.out.println("% of rolls where sum == " + (b+2) + ": " + ((double)a[b]/n)*100);
+        }
+        System.out.println("Your roll is: " + rollDie()); //rollDie method
     }
 }
